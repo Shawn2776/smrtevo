275353a3283ee3476f6e0bb3b93056a13493c721
Shawn Harrington <shawn@2776.ltd>
2024-05-14 13:45:10 -0700

    working on webhooks with clerk

---
+++ app/api/webhooks/clerk/route.js
@@ -0,0 +1,9 @@
+import { clerkClient } from "@clerk/nextjs";
+import { WebhookEvent } from "@clerk/nextjs/server";
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
+import { Webhook } from "svix";
+
+import { createUser } from "../../../lib/actions/UserActions";
+
+export async function POST(req) {}
